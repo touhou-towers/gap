e481a9de7c54c943b5ac1dbe9b6fea4facec255e
reimu <r2hkri@gmail.com>
2019-08-25 22:44:43 +0000

    initial commit

---
+++ src/database.rs
@@ -0,0 +1,90 @@
+use diesel::prelude::*;
+use diesel::dsl::sql;
+use diesel::Queryable;
+use diesel::table;
+
+use dotenv::dotenv;
+
+use serde::Serialize;
+
+use std::env;
+use std::collections::HashMap;
+use std::time::{SystemTime, Duration};
+
+fn connect_to_database() -> MysqlConnection {
+	dotenv().ok();
+
+	let database_url = env::var("DATABASE_URL")
+		.expect("DATABASE_URL was not set!");
+	MysqlConnection::establish(&database_url)
+		.expect(&format!("Couldn't connect to {}", database_url))
+}
+
+pub struct Db {
+	// levelname, then level
+	ballrace_results: HashMap<String, HashMap<usize, String>>,
+	connect: MysqlConnection,
+	updated: SystemTime,
+}
+
+impl Db {
+	pub fn connection() -> Db {
+		Db {
+			ballrace_results: HashMap::new(),
+			connect: connect_to_database(),
+			updated: SystemTime::now(),
+		}
+	}
+
+	pub fn get_ballrace_records(&mut self, map: &str, lvl: usize) -> String {
+		let now = SystemTime::now();
+		if !self.ballrace_results.entry(map.to_string()).or_insert(HashMap::new()).contains_key(&lvl) ||
+			now.duration_since(self.updated).unwrap() >= Duration::new(60 * 60, 0) {
+			let json =
+				serde_json::to_string_pretty(
+					&gm_ballrace::table
+						.filter(gm_ballrace::map.eq(map).and(sql(&format!("lvl = '{}'", lvl))))
+						.order(gm_ballrace::time)
+						.limit(5)
+						.load::<BallraceRecord>(&self.connect)
+						.unwrap()
+				).unwrap();
+			self.ballrace_results.entry(map.to_string())
+				.or_insert(HashMap::new())
+				.insert(lvl, json);
+		}
+		self.ballrace_results.get(map)
+			.unwrap()
+			.get(&lvl)
+			.unwrap()
+			.to_string()
+	}
+}
+
+table! {
+	use diesel::sql_types::*;
+	gm_ballrace {
+		id -> Integer,
+		#[sql_name = "ply"]
+		steam_id64 -> Tinytext,
+		name -> Tinytext,
+		map -> Tinytext,
+		lvl -> Tinytext,
+		time -> Float,
+	}
+}
+
+#[derive(Queryable, Serialize)]
+pub struct BallraceRecord {
+	#[serde(skip_serializing)]
+	pub id: i32,
+	#[serde(default = "unknown")]
+	pub steam_id64: String,
+	#[serde(default = "unknown")]
+	pub name: String,
+	#[serde(default = "unknown")]
+	pub map: String,
+	#[serde(default = "unknown")]
+	pub lvl: String,
+	pub time: f32,
+}
---
+++ src/main.rs
@@ -0,0 +1,52 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+extern crate serde;
+extern crate serde_json;
+
+mod database;
+
+use database::*;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use std::str;
+
+fn main() {
+	let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+	let mut connection = Db::connection();
+
+	for stream in listener.incoming() {
+		let stream = stream.unwrap();
+		handle_connection(stream, &mut connection);
+	}
+}
+
+fn handle_connection(mut stream: TcpStream, connection: &mut Db) {
+	let mut buffer = [0; 512];
+	stream.read(&mut buffer).unwrap();
+
+	let mut req = str::from_utf8(&buffer).unwrap().split_whitespace();
+
+	let response = match [req.next(), req.next(), req.next()] {
+		[Some("GET"), Some(path), Some(_ver)] => {
+			let mut path = path.split("/");
+			path.next(); // starts with a / so get rid of that
+			match [path.next(), path.next(), path.next()] {
+				[Some("ballrace"), Some(map), Some(level)] => {
+					println!("{} {}", map, level);
+					match level.parse::<usize>() {
+						Ok(lvl) => connection.get_ballrace_records(map, lvl),
+						Err(_e) => "{}".to_string(),
+					}
+				},
+				_ => "{}".to_string(),
+			}
+		}
+		_ => "{}".to_string(),
+	};
+
+	let response = format!("{}{}", "HTTP/1.1 200 OK \r\n\r\n", response);
+	stream.write(response.as_bytes()).unwrap();
+	stream.flush().unwrap();
+}
